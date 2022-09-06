3ebf3dd96630448b6fd846e0fdb64ed7cd8d2dbb
Daniel Mattox <dmattox10@gmail.com>
2022-09-06 13:48:11 +0000

    Spelling error...

--- react-sorting-hat/src/components/Message.js
+++ react-sorting-hat/src/components/Message.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Message = props => {
     return(
         <div>
-            <p>Mouse on/off (tap on mobile) the card to flip it'd direction.</p>
+            <p>Mouse on/off (tap on mobile) the card to flip it's direction.</p>
         </div>
     )
 }
